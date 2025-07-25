23620ba57fda1ac06433d408e8631d27540b4775
harikab5 <harikab507@gmail.com>
2025-07-25 12:52:39 +0530

    Delete src directory

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("loggedIn") === "true";
-  return isLoggedIn ? children : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
